0f6d54b53f32aa0eeea2ace5e3324358f23cf525
HCuong <nguyenhungcuong0013@gmail.com>
2024-10-02 22:24:55 +0700

    registe demo"

---
+++ src/components/Login.jsx
@@ -0,0 +1,76 @@
+// Login.jsx
+import React from 'react';
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBCheckbox,
+  MDBIcon
+} from 'mdb-react-ui-kit';
+import '../styles/Login.css';
+
+function Login() {
+  return (
+    <MDBContainer fluid className='p-5 background-radial-gradient overflow-hidden' style={{ overflow: 'hidden' }}>
+      <MDBRow>
+        <MDBCol md='6' className='text-center text-md-start d-flex flex-column justify-content-center'>
+          <h1 className="my-5 display-3 fw-bold ls-tight px-3" style={{ color: 'hsl(218, 81%, 95%)' }}>
+            Luxry <br />
+            <span style={{ color: 'hsl(218, 81%, 75%)' }}>Vista Resort</span>
+          </h1>
+          <p className='px-3' style={{ color: 'hsl(218, 81%, 85%)' }}>
+          Our website is specially designed to provide the perfect hotel management experience for upscale resorts. 
+          With a user-friendly interface, you can easily manage bookings, track room availability, and oversee customer interactions,
+           all while enhancing your guest services. Our system integrates advanced technology to streamline operations 
+           and elevate the customer experience. At Luxury Resort Management, you will discover how to turn every stay into an unforgettable experience for your guests.
+          </p>
+        </MDBCol>
+
+        <MDBCol md='6' className='position-relative'>
+          <div id="radius-shape-1" className="position-absolute rounded-circle shadow-5-strong"></div>
+          <div id="radius-shape-2" className="position-absolute shadow-5-strong"></div>
+
+          <MDBCard className='my-5 bg-glass'>
+            <MDBCardBody className='p-5'>
+              <MDBRow>
+                <MDBCol col='6'>
+                  <MDBInput wrapperClass='mb-4' label='First name' id='form1' type='text' />
+                </MDBCol>
+                <MDBCol col='6'>
+                  <MDBInput wrapperClass='mb-4' label='Last name' id='form2' type='text' />
+                </MDBCol>
+              </MDBRow>
+              <MDBInput wrapperClass='mb-4' label='Email' id='form3' type='email' />
+              <MDBInput wrapperClass='mb-4' label='Password' id='form4' type='password' />
+              <div className='d-flex justify-content-center mb-4'>
+                <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember password' />
+              </div>
+              <MDBBtn className='w-100 mb-4' size='md'>Sign Up</MDBBtn>
+              <div className="text-center">
+                <p>or sign up with:</p>
+                <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
+                  <MDBIcon fab icon='facebook-f' size="sm" />
+                </MDBBtn>
+                <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
+                  <MDBIcon fab icon='twitter' size="sm" />
+                </MDBBtn>
+                <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
+                  <MDBIcon fab icon='google' size="sm" />
+                </MDBBtn>
+                <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
+                  <MDBIcon fab icon='github' size="sm" />
+                </MDBBtn>
+              </div>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+}
+
+export default Login;
