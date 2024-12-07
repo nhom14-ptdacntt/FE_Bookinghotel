6f5d2d8999286564c62498dc1f9a94352c8c024b
Huy Hoàng <tranhuyyhoang2004@gmail.com>
2024-12-07 19:38:15 +0700

    build: call api in signin ,signup and validate data

--- src/App.jsx
+++ src/App.jsx
@@ -12,18 +12,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './styles/app.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   return (
     <Router>
       <div>
+      <ToastContainer />
         <Routes>
           <Route path="/" element={<div>Welcome to Hotel Booking</div>} />
           <Route path="/home" element={<div><Home/></div>} /> 
           <Route path="/signup" element={<div className="custom-background"><SignUp/></div>} /> 
           <Route path="/signin" element={<div className="custom-background"><SignIn/></div>} /> 
           <Route path="/booking" element={<BookingManagement />} />
-          <Route path="/rooms" element={<RoomManagement />} />
+          <Route path="/dashboard" element={<RoomManagement />} />
           
         </Routes>
       </div>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 
-import React from 'react';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
 import {
   MDBBtn,
   MDBContainer,
@@ -12,8 +13,122 @@ import {
   MDBIcon
 } from 'mdb-react-ui-kit';
 import '../styles/Login.css';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function Signup() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  })
+
+  const navigate = useNavigate()
+
+  const handleSignup = () => {
+    if (validate()) {
+      const fullName = `${firstName.trim()} ${lastName.trim()}`;
+      const user = {
+        fullName: fullName,
+        email: email.trim(),
+        password: password.trim()
+      }
+      signUp(user)
+    } 
+   
+  }
+
+  const signUp = async (user) => {
+    try {
+      const res = await axios.post('http://localhost:8080/api/v1/create_user', user);
+      console.log(res.data)
+      toast.success('Đăng ký thành công!', {
+        position: 'top-right',
+        autoClose: 3000 // 3 giây
+      });
+
+      // Điều hướng sang trang đăng nhập sau 3 giây
+      setTimeout(() => {
+        navigate('/signin');
+      }, 3000);
+    } catch (error) {
+      console.log(error)
+      const newErrors = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: ''
+      };
+      if(error.response.data.code === 1002) {
+        newErrors.email = "Email đã tồn tại!!";
+      }
+      setErrors(newErrors);
+
+      // Hiển thị toast lỗi
+      toast.error('Đăng ký thất bại!', {
+        position: 'top-right',
+        autoClose: 3000 // 3 giây
+      });
+    }
+  }
+
+  
+  const validate = () => {
+    let isValid = true;
+    const newErrors = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: ''
+    };
+  
+    // Kiểm tra các trường không được bỏ trống
+    if (!firstName.trim()) {
+      newErrors.firstName = 'First name is required';
+      isValid = false;
+    }
+    if (!lastName.trim()) {
+      newErrors.lastName = 'Last name is required';
+      isValid = false;
+    }
+    if (!email.trim()) {
+      newErrors.email = 'Email is required';
+      isValid = false;
+    } else {
+      // Kiểm tra định dạng email
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(email)) {
+        newErrors.email = 'Invalid email format';
+        isValid = false;
+      }
+    }
+    if (!password.trim()) {
+      newErrors.password = 'Password is required';
+      isValid = false;
+    }
+  
+    setErrors(newErrors);
+    return isValid;
+  };
+  
+  const handleChangeFirstName = (e) => {
+    setFirstName(e.target.value)
+    
+  }
+  const handleChangeLastName = (e) => {
+    setLastName(e.target.value)
+  }
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value)
+  }
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value)
+  }
   return (
     <MDBContainer fluid className='p-5 background-radial-gradient overflow-hidden' style={{ overflow: 'hidden' }}>
       <MDBRow>
@@ -38,18 +153,22 @@ function Signup() {
             <MDBCardBody className='p-5'>
               <MDBRow>
                 <MDBCol col='6'>
-                  <MDBInput wrapperClass='mb-4' label='First name' id='form1' type='text' />
+                  <MDBInput onChange={handleChangeFirstName} wrapperClass='mb-4' label='First name' id='form1' type='text' />
+                  {errors.firstName && <p className="text-danger">{errors.firstName}</p>}
                 </MDBCol>
                 <MDBCol col='6'>
-                  <MDBInput wrapperClass='mb-4' label='Last name' id='form2' type='text' />
+                  <MDBInput onChange={handleChangeLastName} wrapperClass='mb-4' label='Last name' id='form2' type='text' />
+                  {errors.lastName && <p className="text-danger">{errors.lastName}</p>}
                 </MDBCol>
               </MDBRow>
-              <MDBInput wrapperClass='mb-4' label='Email' id='form3' type='email' />
-              <MDBInput wrapperClass='mb-4' label='Password' id='form4' type='password' />
+              <MDBInput onChange={handleChangeEmail} wrapperClass='mb-4' label='Email' id='form3' type='email' />
+              {errors.email && <p className="text-danger">{errors.email}</p>}
+              <MDBInput onChange={handleChangePassword} wrapperClass='mb-4' label='Password' id='form4' type='password' />
+              {errors.password && <p className="text-danger">{errors.password}</p>}
               <div className='d-flex justify-content-center mb-4'>
                 <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember password' />
               </div>
-              <MDBBtn className='w-100 mb-4' size='md'>Sign Up</MDBBtn>
+              <MDBBtn onClick={handleSignup} className='w-100 mb-4' size='md'>Sign Up</MDBBtn>
               <div className="text-center">
                 <p>or sign up with:</p>
                 <MDBBtn tag='a' color='none' className='mx-3' style={{ color: '#1266f1' }}>
